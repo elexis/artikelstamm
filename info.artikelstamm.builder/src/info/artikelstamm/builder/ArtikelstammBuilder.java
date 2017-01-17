9889f55ee84bd5313300153ad8b37423b295bb67
Marco Descher <marco@descher.at>
2017-01-17 08:54:31 +0100

    Update docu

--- info.artikelstamm.builder/src/info/artikelstamm/builder/ArtikelstammBuilder.java
+++ info.artikelstamm.builder/src/info/artikelstamm/builder/ArtikelstammBuilder.java
@@ -7,7 +7,6 @@ import java.util.Collections;
 import java.util.Comparator;
 import java.util.Date;
 import java.util.List;
-import java.util.Objects;
 
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
