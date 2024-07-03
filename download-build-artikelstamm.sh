f1748b505965792fef366d388f60792db1e0a623
thuster <huster@medevit.at>
2024-07-03 11:36:41 +0200

    [26457] use version 0.2 to build medindex artikelstamm

--- download-build-artikelstamm.sh
+++ download-build-artikelstamm.sh
@@ -1,15 +1,17 @@
 #!/usr/bin/env bash
 rm -f MedIndexDownload.jar ArtikelstammBuilder.jar Elexis_Artikelstamm_v5.xsd
 rm -Rf medindex/
-wget https://github.com/elexis/artikelstamm/releases/download/0.1/MedIndexDownload.jar
-wget https://github.com/elexis/artikelstamm/releases/download/0.1/ArtikelstammBuilder.jar
-wget https://raw.githubusercontent.com/elexis/artikelstamm/master/info.artikelstamm.model/lib/Elexis_Artikelstamm_v5.xsd
+wget https://github.com/elexis/artikelstamm/releases/download/0.2/MedIndexDownload.jar
+wget https://github.com/elexis/artikelstamm/releases/download/0.2/ArtikelstammBuilder.jar
+wget https://github.com/elexis/artikelstamm/releases/download/0.2/Elexis_Artikelstamm_v5.xsd
 
 docker run --rm -v "$(pwd)":/prog -w /prog adoptopenjdk/openjdk8:alpine java -XX:MaxRAMPercentage=75.0 -jar MedIndexDownload.jar ${HCI_USERNAME} ${HCI_PASSWORD}
 
 docker run --rm -v "$(pwd)":/prog -w /prog adoptopenjdk/openjdk8:alpine java -XX:MaxRAMPercentage=75.0  -jar ArtikelstammBuilder.jar \
 -emediatProductFile medindex/medindex_product.xml \
--emediatArticleFile medindex/medindex_article.xml -emediatCodeFile medindex/medindex_code.xml \
+-emediatArticleFile medindex/medindex_article.xml \
+-emediatArticlePriceFile medindex/medindex_article_price.xml \
+-emediatCodeFile medindex/medindex_code.xml \
 -emediatLimitationFile medindex/medindex_limitation.xml \
 -artikelstammSchema Elexis_Artikelstamm_v5.xsd
 
